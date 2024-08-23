ef1e55feeea43d39e0950757daa6b4a190365f59
Joao Garcia <jpmldg@gmail.com>
2024-08-23 10:55:29 -0300

    Removed confing comments

--- cron-job/graphql/config.js
+++ cron-job/graphql/config.js
@@ -2,8 +2,6 @@ const graphqlConfig = {
     endpoint: 'http://localhost:8080/graphql',
     headers: {
         'Content-Type': 'application/json',
-        // You can add authentication tokens or other headers here if needed
-        // 'Authorization': `Bearer YOUR_TOKEN_HERE`,
     }
 };
 
